1045d2e48150383094acf41cc92199f3959ef1db
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2024-07-11 16:34:17 +0200

    assigning values​to variables

--- Lesson 1/Task 1/main-1.py
+++ Lesson 1/Task 1/main-1.py
@@ -41,3 +41,25 @@ class Cart:
                             zip(self.__products, self.__quantities)))
         res += f"\nTotal: {self.total()} UAH"
         return res
+
+
+pr_1 = Product("Bread", 10)
+pr_2 = Product("Milk", 20)
+pr_3 = Product("Butter", 30)
+
+cart_1 = Cart()
+cart_1.add_product(pr_1, 2)
+cart_1.add_product(pr_2, 3)
+cart_1.add_product(pr_3)
+cart_1.add_product(pr_3, 3)
+
+pr_4 = Product("\nHoney", 20)
+pr_5 = Product("Meat", 30)
+
+cart_2 = Cart()
+cart_2.add_product(pr_4, 1)
+cart_2.add_product(pr_5, 3)
+
+cart_1 += cart_2
+
+print(cart_1)
