b65f4266ba866f996f11056367385083dbfd0ed3
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2024-07-11 16:38:54 +0200

    add class Category

--- Lesson 1/Task 2/main-2.py
+++ Lesson 1/Task 2/main-2.py
@@ -8,3 +8,16 @@ class Dish:
 
     def __str__(self):
         return f"{self.name} - {self.price} UAH"
+
+
+class Category:
+    def __init__(self, name: str):
+        self.name = name
+        self.__dishes = []
+
+    def add_dish(self, dish: Dish):
+        self.__dishes.append(dish)
+
+    def __str__(self):
+        dishes = "\n".join(map(str, self.__dishes))
+        return f"{self.name}\n{dishes}"
