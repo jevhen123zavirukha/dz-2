ea33fd1825174468dc7bfc42ea0d1cf8c6e376b1
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2024-07-11 16:47:30 +0200

    add class ProperFraction

--- Lesson 1/Task 3/main-3.py
+++ Lesson 1/Task 3/main-3.py
@@ -1,2 +1,41 @@
 # Task 3
 import math
+
+
+class ProperFraction:
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+        self.denominator = denominator
+        self.reduce_fraction()
+
+    def __str__(self):
+        return f'{self.numerator}/{self.denominator}'
+
+    def reduce_fraction(self):
+        gcd = math.gcd(self.numerator, self.denominator)
+        self.numerator //= gcd
+        self.denominator //= gcd
+
+    def __lt__(self, other):
+        return self.numerator * other.denominator < other.numerator * self.denominator
+
+    def __gt__(self, other):
+        return self.numerator * other.denominator > other.numerator * self.denominator
+
+    def __eq__(self, other):
+        return self.numerator == other.numerator and self.denominator == other.denominator
+
+    def __add__(self, other):
+        new_numerator = self.numerator * other.denominator + other.numerator * self.denominator
+        new_denominator = self.denominator * other.denominator
+        return ProperFraction(new_numerator, new_denominator)
+
+    def __sub__(self, other):
+        new_numerator = self.numerator * other.denominator - other.numerator * self.denominator
+        new_denominator = self.denominator * other.denominator
+        return ProperFraction(new_numerator, new_denominator)
+
+    def __mul__(self, other):
+        new_numerator = self.numerator * other.numerator
+        new_denominator = self.denominator * other.denominator
+        return ProperFraction(new_numerator, new_denominator)
