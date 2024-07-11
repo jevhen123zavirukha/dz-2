2b66982a6c7a3233b51638dbc28a132f292a81ed
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2024-07-11 16:29:42 +0200

    add class Product

---
+++ Lesson 1/Task 1/main-1.py
@@ -0,0 +1,10 @@
+# Task 1
+
+
+class Product:
+    def __init__(self, name: str, price: int | float):
+        self.name = name
+        self.price = price
+
+    def __str__(self):
+        return f"{self.name}: {self.price} UAH"
