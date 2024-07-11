b64df6d230771698ee08aaca6b1d5f5957e37bdf
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2024-07-11 16:31:15 +0200

    add class Cart

--- Lesson 1/Task 1/main-1.py
+++ Lesson 1/Task 1/main-1.py
@@ -8,3 +8,36 @@ class Product:
 
     def __str__(self):
         return f"{self.name}: {self.price} UAH"
+
+
+class Cart:
+
+    def __init__(self):
+        self.__products = []
+        self.__quantities = []
+
+    def add_product(self, product: Product, quantity: int | float = 1):
+        if product in self.__products:
+            index = self.__products.index(product)
+            self.__quantities[index] += quantity
+        else:
+            self.__products.append(product)
+            self.__quantities.append(quantity)
+
+    def total(self):
+        res = 0
+        for product, quantity in zip(self.__products, self.__quantities):
+            res += product.price * quantity
+        return res
+
+    def __iadd__(self, other):
+        if isinstance(other, Cart):
+            self.__products.extend(other.__products)
+            self.__quantities.extend(other.__quantities)
+        return self
+
+    def __str__(self):
+        res = '\n'.join(map(lambda item: f"{item[0]} x {item[1]} = {item[0].price * item[1]} UAH",
+                            zip(self.__products, self.__quantities)))
+        res += f"\nTotal: {self.total()} UAH"
+        return res
