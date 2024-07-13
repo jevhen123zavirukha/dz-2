1659f6896bd67f6cf88fe50b063b0d54bbc865e1
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2024-07-13 17:16:54 +0200

    add class Cart

--- Lesson 2/Task 1/main_1.py
+++ Lesson 2/Task 1/main_1.py
@@ -44,3 +44,53 @@ class Product:
 
     def __str__(self):
         return f"{self.name}: {self.price} UAH"
+
+
+class Cart:
+    def __init__(self):
+        self.__products = {}
+
+    def __iter__(self):
+        self.__iter_index = 0
+        return self
+
+    def __next__(self):
+        products = list(self.__products.keys())
+        if self.__iter_index < len(products):
+            product = products[self.__iter_index]
+            quantity = self.__products[product]
+            self.__iter_index += 1
+            return product, quantity
+        else:
+            raise StopIteration
+
+    def add_product(self, product: Product, quantity: int | float):
+        """
+        Add product to the cart
+        :param product: product to add
+        :param quantity: quantity of the product
+        :return: None
+        """
+        if not isinstance(quantity, int | float):
+            logger.error("Quantity must be an integer or float")
+            raise TypeError("Quantity must be an integer or float")
+        if quantity <= 0:
+            logger.error("Quantity must be greater than 0")
+            raise ValueError("Quantity must be greater than 0")
+        if not isinstance(product, Product):
+            logger.error("Product must be an instance of Product class")
+            raise TypeError("Product must be an instance of Product class")
+        logger.info(f"Added {product} x {quantity}")
+        self.__products[product] = self.__products.get(product, 0) + quantity
+
+    def total(self):
+        """
+        Calculate total price of the cart
+        :return: sum of the prices of all products in the cart
+        """
+        total_price = sum(product.price * quantity for product, quantity in self.__products.items())
+        return total_price
+
+    def __str__(self):
+        return "\n".join(f"{product} x {quantity} = {quantity * product.price}"
+                         for product, quantity in self.__products.items()) + f"\nTotal: {self.total()} UAH"
