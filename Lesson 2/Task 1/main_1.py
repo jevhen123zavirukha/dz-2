6cc23779995141a192bcf7916423a3c1e7caeeae
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2024-07-13 17:16:08 +0200

    add class Product

--- Lesson 2/Task 1/main_1.py
+++ Lesson 2/Task 1/main_1.py
@@ -20,3 +20,27 @@ logger.addHandler(stream_handler)
 class PriceError(Exception):
     def __init__(self, message):
         super().__init__(message)
+
+
+class Product:
+    """
+    Class for product
+    """
+    def __init__(self, name: str, price: int | float):
+        """
+        Initialize the product
+        :param name: name of the product
+        :param price: price of the product
+        """
+        if not isinstance(price, int | float):
+            logger.error("Price must be an integer or float")
+            raise TypeError("Price must be an integer or float")
+        if price <= 0:
+            logger.error("Price must be greater than 0")
+            raise PriceError("Price must be greater than 0")
+
+        self.name = name
+        self.price = price
+
+    def __str__(self):
+        return f"{self.name}: {self.price} UAH"
