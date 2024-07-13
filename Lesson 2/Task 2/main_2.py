6f177db7e74becbb3a265e4d94f085bc442d2a2b
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2024-07-13 17:23:35 +0200

    add class Menu

--- Lesson 2/Task 2/main_2.py
+++ Lesson 2/Task 2/main_2.py
@@ -19,3 +19,26 @@ class Category:
     def __str__(self):
         dishes = "\n".join(map(str, self.__dishes))
         return f"{self.name}\n{dishes}"
+
+
+class Menu:
+    def __init__(self):
+        self.__categories = []
+        self.__index = 0
+
+    def add_category(self, category: Category):
+        self.__categories.append(category)
+
+    def __str__(self):
+        return "\n".join(map(str, self.__categories))
+
+    def __iter__(self):
+        self.__index = 0
+        return self
+
+    def __next__(self):
+        if self.__index < len(self.__categories):
+            category = self.__categories[self.__index]
+            self.__index += 1
+            return category
+        raise StopIteration
