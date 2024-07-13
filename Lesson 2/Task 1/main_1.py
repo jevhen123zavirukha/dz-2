4aa9c2e6dc051d81e0cdd0f181fe6754d7cd2d9f
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2024-07-13 17:14:33 +0200

    import

--- Lesson 2/Task 1/main_1.py
+++ Lesson 2/Task 1/main_1.py
@@ -1,1 +1,17 @@
 # Task 1
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+
+file_handler = logging.FileHandler('main.log')
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.ERROR)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+stream_handler.setLevel(logging.DEBUG)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
