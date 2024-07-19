e4a306e06de11306a278a810847d627071677fb0
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2024-07-19 20:59:00 +0200

    assigning values to variables and displaying on the screen

--- Lesson 3/Task 1/main-1.py
+++ Lesson 3/Task 1/main-1.py
@@ -7,3 +7,11 @@ def geometric_progression(a, r):
     while True:
         yield current_term
         current_term *= r
+
+
+a = 2
+r = 3
+progression = geometric_progression(a, r)
+
+for _ in range(5):
+    print(next(progression))
