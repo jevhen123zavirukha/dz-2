bd61aa084beb3bb8640c336b89e80e32bf9903c7
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2024-07-20 21:12:36 +0200

    displaying on the screen

--- Lesson 3/Task 2/main-2.py
+++ Lesson 3/Task 2/main-2.py
@@ -9,3 +9,11 @@ def custom_range(start, stop=None, step=1):
     while (step > 0 and current < stop) or (step < 0 and current > stop):
         yield current
         current += step
+
+
+print("\n")
+
+for i in custom_range(int(input("Enter start: ")), int(input("Enter stop: ")), int(input("Enter step: "))):
+    print(f"{i}")
+
+print("\n")
