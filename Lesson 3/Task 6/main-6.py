2c9bdf17894d3c2dcea9defa3e6396c02b8ecc79
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2024-07-20 21:45:56 +0200

    add function generator

---
+++ Lesson 3/Task 6/main-6.py
@@ -0,0 +1,9 @@
+# Task 6
+from _datetime import datetime, timedelta
+
+
+def date_range(start_date, end_date):
+    current_date = start_date
+    while current_date <= end_date:
+        yield current_date
+        current_date += timedelta(days=1)
