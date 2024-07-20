396d762836ff34013bf6adeb18ca8b40a781beee
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2024-07-20 21:16:04 +0200

    add function generator

---
+++ Lesson 3/Task 3/main-3.py
@@ -0,0 +1,14 @@
+# Task 3
+
+
+def prime_generator(limit):
+    primes = []
+    for num in range(2, limit + 1):
+        is_prime = True
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(num)
+            yield num
