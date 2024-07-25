3e5411013a5c54f116a3c21eb75e3026238c6cf3
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2024-07-25 15:54:15 +0200

    add decorator

---
+++ Lesson 5/Task 4/main-4.py
@@ -0,0 +1,15 @@
+# Task 4
+
+
+import time
+
+
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"Function execution time : {execution_time:.3f} seconds")
+        return result
+    return wrapper
