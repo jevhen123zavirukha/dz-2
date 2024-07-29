89a9205bd74bc60ea967d2c3253de64737b77308
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2024-07-29 20:24:02 +0200

    add class BankTransfer

--- Lesson 6/Task 2.py
+++ Lesson 6/Task 2.py
@@ -11,3 +11,8 @@ class PaymentMedia(ABC):
 class CreditCard(PaymentMedia):
     def implement_a_payment(self, amount):
         print(f"Payment {amount} UAH. made using a credit card.")
+
+
+class BankTransfer(PaymentMedia):
+    def implement_a_payment(self, amount):
+        print(f"Payment {amount} UAH. made using a bank transfer.")
