5a04a197c450fde7621093bacef6bf44509846b7
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2024-07-29 20:25:57 +0200

    add class PaymentProcessor

--- Lesson 6/Task 2.py
+++ Lesson 6/Task 2.py
@@ -22,3 +22,17 @@ class OnlineWallet(PaymentMedia):
     def implement_a_payment(self, amount):
         print(f"Payment {amount} UAH. made using a online wallet.")
 
+
+class PaymentProcessor:
+    def __init__(self):
+        self.payment_methods = []
+
+    def add_payment_method(self, method):
+        self.payment_methods.append(method)
+
+    def make_a_payment(self, method_index, amount):
+        if method_index < len(self.payment_methods):
+            method = self.payment_methods[method_index]  # Fixed variable name
+            method.implement_a_payment(amount)
+        else:
+            print("The selected payment method does not exist.")
