6998071f384994be90efe7cb10b2ab7691666fcc
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2024-07-29 20:19:50 +0200

    add class Rectangle

--- Lesson 6/Task 1/main-1.py
+++ Lesson 6/Task 1/main-1.py
@@ -22,3 +22,15 @@ class Circle(Figure):
 
     def perimeter(self):
         return 2 * math.pi * self.radius
+
+
+class Rectangle(Figure):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
