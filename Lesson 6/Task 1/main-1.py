c13baf26cb94fcaab30a09ab1b7073ebb13e9bbd
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2024-07-29 20:19:15 +0200

    add class Circle

--- Lesson 6/Task 1/main-1.py
+++ Lesson 6/Task 1/main-1.py
@@ -11,3 +11,14 @@ class Figure(ABC):
     @abstractmethod
     def perimeter(self):
         pass
+
+
+class Circle(Figure):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
